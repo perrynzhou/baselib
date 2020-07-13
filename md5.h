1cb9337035374c327a942323fcd0200c4ac884da
perrynzhou <perrynzhou@gmail.com>
2020-07-13 16:50:56 +0800

    add hash functions lib

---
+++ md5.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+  > File Name: md5.h
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: 2020年04月22日 星期三 22时48分36秒
+ ************************************************************************/
+
+#ifndef _CSTL_MD5_H
+#define _CSTL_MD5_H
+int cstl_md5_string(unsigned char *dest_str, unsigned int dest_len, char *md5_str);
+int cst_md5_file(const char *file_path, char *md5_str);
+#endif
