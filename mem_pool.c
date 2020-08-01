71581048d74a963cd021f6ee4f0e328a3b19436e
perrynzhou <perrynzhou@gmail.com>
2020-08-01 11:51:12 +0800

    init mem pool

---
+++ mem_pool.c
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: mem_pool.c
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: Saturday, August 01, 2020 AM11:22:36
+ ************************************************************************/
+
+#include<stdio.h>
+
+
---
+++ mem_pool.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+    > File Name: mem_pool.h
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: Saturday, August 01, 2020 AM11:22:32
+ ************************************************************************/
+
+#ifndef _MEM_POOL_H
+#define _MEM_POOL_H
+#include <stdint.h>
+#include <stdio.h>
+struct cstl_mem_pool_t;
+typedef struct cstl_mem_pool_large_t
+{
+  struct cstl_mem_pool_large_t *next;
+  void *data;
+} cstl_mem_pool_large;
+typedef struct cstl_mem_pool_data_t
+{
+  uint8_t *start;
+  uint8_t *end;
+  struct cstl_mem_pool_t *next;
+} cstl_mem_pool_data;
+typedef struct cstl_mem_pool_t
+{
+  cstl_mem_pool_data d;
+  struct cstl_mem_pool_t *current;
+  struct cstl_mem_pool_large_t *large;
+} cstl_mem_pool;
+
+cstl_mem_pool *cstl_mem_pool_create(size_t size);
+void *cstl_mem_pool_alloc(cstl_mem_pool *pool, size_t size);
+void cstl_mem_pool_free(cstl_mem_pool *pool);
+#endif
