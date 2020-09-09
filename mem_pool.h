5106d363796bacc9f6c31daadcc73d4d17c1a1b1
perrynzhou <perrynzhou@gmail.com>
2020-09-09 17:04:26 +0800

    add log impl

--- mem_pool.h
+++ mem_pool.h
@@ -9,28 +9,28 @@
 #define _MEM_POOL_H
 #include <stdint.h>
 #include <stdio.h>
-struct cstl_mem_pool_t;
-typedef struct cstl_mem_pool_large_t
+struct mem_pool_t;
+typedef struct mem_pool_large_t
 {
-  struct cstl_mem_pool_large_t *next;
+  struct mem_pool_large_t *next;
   void *alloc;
-} cstl_mem_pool_large;
-typedef struct cstl_mem_pool_data_t
+} mem_pool_large;
+typedef struct mem_pool_data_t
 {
   uint8_t *start;
   uint8_t *end;
-  struct cstl_mem_pool_t *next;
-} cstl_mem_pool_data;
-typedef struct cstl_mem_pool_t
+  struct mem_pool_t *next;
+} mem_pool_data;
+typedef struct mem_pool_t
 {
-  cstl_mem_pool_data d;
+  mem_pool_data d;
   //if alloc memory overt max_alloc_size,use large to link big memory
   size_t max_alloc_size;
-  struct cstl_mem_pool_t *current;
-  struct cstl_mem_pool_large_t *large;
-} cstl_mem_pool;
+  struct mem_pool_t *current;
+  struct mem_pool_large_t *large;
+} mem_pool;
 
-cstl_mem_pool *cstl_mem_pool_create(size_t max_alloc_size, size_t alloc_size);
-void *cstl_mem_pool_alloc(cstl_mem_pool *pool, size_t size);
-void cstl_mem_pool_free(cstl_mem_pool *pool);
+mem_pool *mem_pool_create(size_t max_alloc_size, size_t alloc_size);
+void *mem_pool_alloc(mem_pool *pool, size_t size);
+void mem_pool_free(mem_pool *pool);
 #endif
