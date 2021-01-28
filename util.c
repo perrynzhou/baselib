236c4c2f784015be4621e70313275312e2869ffb
perrynzhou <perrynzhou@gmail.com>
2021-01-28 08:24:08 +0800

    add string convertion func

---
+++ util.c
@@ -0,0 +1,8 @@
+/*************************************************************************
+    > File Name: util.c
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: Thursday, January 28, 2021 AM08:16:44
+ ************************************************************************/
+
+#include <stdio.h>
